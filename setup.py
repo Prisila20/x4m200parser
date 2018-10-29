6804ff872d21aa97cd12cd9e45e27736aebec0bd
Geir Kulia <geir@kulia.no>
2018-10-29 16:58:23 +0100

    Add setup script

---
+++ setup.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='x4m300parser',
+      version='0.1',
+      description='Parser fro x4m300',
+      author='Geir Kulia',
+      author_email='geir@kulia.no',
+      url='http://kulia.no',
+      packages=['x4m300parser'],
+     )
